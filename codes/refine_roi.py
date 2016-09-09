23251424fc7f563a10e2bc38cc764babd2d763f1
Yancy <chen_yang@zju.edu.cn>
2016-09-09 17:08:50 -0400

    merge roidb

--- codes/refine_roi.py
+++ codes/refine_roi.py
@@ -1,25 +1,99 @@
 import os
 
-root = '/home/chenyang/Results/'
-add_path = '/home/chenyang/sed/data/Annotations/refine_jiac/'
+root = '/home/chenyang/refine/'
+classes = ['CellToEar', 'Pointing', 'Embrace']
 roi_path = '/home/chenyang/sed/data/Annotations/refine/'
+
+threhold = 0.8
+def same_roi(roi_a, roi_b):
+    #if roi_a != roi_b:
+        #print 'a', roi_a
+        #print 'b', roi_b
+    if roi_a[0] != roi_b[0]:
+        return False
+    ax0 = int(roi_a[1])
+    ay0 = int(roi_a[2])
+    ax1 = int(roi_a[3])
+    ay1 = int(roi_a[4])
+
+    bx0 = int(roi_b[1])
+    by0 = int(roi_b[2])
+    bx1 = int(roi_b[3])
+    by1 = int(roi_b[4])
+
+    if ax0 <= bx0 and ax1 >= bx1 and ay0 <= by0 and ay1 >= by1:
+        return True
+    if bx0 <= ax0 and bx1 >= ax1 and by0 <= ay0 and by1 >= ay1:
+        return True
+
+    Intersect = (min(ax1, bx1) - max(ax0,bx0)) * (min(ay1, by1) - max(ay0,by0))
+    if Intersect < 0:
+        return False
+    Union = (max(ax1, bx1) - min(ax0,bx0)) * (max(ay1, by1) - min(ay0,by0))
+
+    #print float(Intersect) / float(Union)
+    return float(Intersect) / float(Union) > threhold
+
+def check_dupl(roi_file, new_roi):
+    with open(roi_file) as f:
+        rois = [x.strip().split(' ') for x in f.readlines()]
+    for roi in rois:
+        if same_roi(roi, new_roi):
+            return True
+    return False
 
 def refine_roi(src_path, roi_path):
     files = os.listdir(src_path)
+    total = 0
     count = 0
     for src in files:
+        total = total + 1
         name, ext = os.path.splitext(src)
         data = name.split('_')
         img_name = '_'.join(data[:5])
 
-        roi_file = os.path.join(roi_path, img_name + roi)
-        os.path.exists(roi_file)
-        with open(roi_file, 'a') as f:
-            f.write(' '.join(data[5:]))
+        roi_file = os.path.join(roi_path, img_name + '.roi')
+        #print roi_file
+        if os.path.exists(roi_file):
+            if check_dupl(roi_file, data[5:]):
+                continue
+            print roi_file
+            with open(roi_file, 'a') as f:
+                f.write(' '.join(data[5:]) + '\n')
+        else:
+            with open(roi_file, 'w') as f:
+                f.write(' '.join(data[5:]) + '\n')
         count = count + 1
+    print 'Total:', total
     print 'Roi written:', count
+
+
+train_set = '/home/chenyang/lib/ImageSets/refine_train.txt'
+test_set = '/home/chenyang/lib/ImageSets/refine_test.txt'
+def generate_imgset():
+    print roi_path
+    annos = os.listdir(roi_path)
+    with open(train_set, 'w') as train, open(test_set, 'w') as test:
+        total_test = 0
+        total_train = 0
+        for anno in annos:
+            name, ext = os.path.splitext(anno)
+            data = name.split('_')
+            date = int(data[1][-4:])
+            if date > 1201:
+                total_test = total_test + 1
+                test.write(name + '\n')
+            else:
+                total_train = total_train + 1
+                train.write(name + '\n')
+        print 'Total train: ', total_train
+        print 'Total test: ', total_test
+
 
 if __name__ == '__main__':
     for cls in classes:
+        print cls
         src_path = os.path.join(root, cls, 'refined')
         refine_roi(src_path, roi_path)
+    generate_imgset()
+    
---
+++ codes/roi_process.py
@@ -0,0 +1,75 @@
+import os
+import re
+
+anno_path = '/home/chenyang/sed/data/Annotations/'
+#roi_dbs = ['raw_bbox', 'pose_roi']
+roi_dbs = ['refine', 'pose_roi']
+final_path = '/home/chenyang/sed/data/Annotations/roi/'
+
+def merge_roidb(roi_dbs):
+    for roi_db in roi_dbs:
+        roi_path = os.path.join(anno_path, roi_db)
+        roi_files = os.listdir(roi_path)
+        for roi_file in roi_files:
+            f_roi = open(os.path.join(roi_path, roi_file))
+            final_roi = os.path.join(final_path, roi_file)
+            if os.path.exists(final_roi):
+                print roi_file
+            with open(final_roi, 'a') as f:
+                f.write(f_roi.read())
+
+files = ['/home/chenyang/lib/ImageSets/train.txt', '/home/chenyang/lib/ImageSets/test.txt']
+src = ['/home/chenyang/sed/data/Annotations/refine/', '/home/chenyang/sed/data/Annotations/pose_roi/'] 
+dst = '/home/chenyang/sed/data/Annotations/roi/'
+def extract_roidb(files, src, dst):
+    for img_set in files:
+        with open(img_set) as f:
+            imgs = [x.strip() for x in f.readlines()]
+        for img in imgs:
+            found = 0
+            for roidb in src:
+                src_roi = os.path.join(roidb, img + '.roi')
+                if not os.path.exists(src_roi):
+                    continue
+                found = found + 1
+
+                src_file = open(src_roi)
+                roi = os.path.join(dst, img + '.roi')
+                if os.path.exists(roi):
+                    print img    
+                with open(roi, 'a') as f:
+                    f.write(src_file.read())
+
+            if found == 0:
+                print 'Not find roi of image {} from src'.format(img)
+
+def filter_roidb(dst):
+    roidbs = os.listdir(dst)
+    for roidb in roidbs:
+        roi_file = os.path.join(dst, roidb)
+        with open(roi_file) as f:
+            rois = [list(x) for x in re.findall('(\S+) (\d+) (\d+) (\d+) (\d+)', f.read())]
+        change = False
+        for roi in rois:
+            if int(roi[1]) > 719:
+                change = True
+                roi[1] = '719'
+            if int(roi[2]) > 575:
+                change = True
+                roi[2] = '575'
+            if int(roi[3]) > 719:
+                change = True
+                roi[3] = '719'
+            if int(roi[4]) > 757: 
+                change = True
+                roi[4] = '575'
+        if change:
+            print roidb
+            with open(roi_file, 'w') as f:
+                for roi in rois:
+                    f.write(' '.join(roi) + '\n')
+
+if __name__ == '__main__':
+    #merge_roidb(roi_dbs)
+    #extract_roidb(files, src, dst)
+    filter_roidb(dst)
